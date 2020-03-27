d6c7a81a3409498fd6f2707b6aaea3b11c788abd
ashar ali <ashar20@live.com>
2020-03-27 15:14:58 +0500

    update

--- lib/globarl.dart
+++ lib/globarl.dart
@@ -3,20 +3,28 @@ import 'dart:convert';
 
 import 'package:contacts_service/contacts_service.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class Abc {
   static List<Contact> contacts;
   static Map<String, int> statusFlag;
   static HashSet customerSet = new HashSet();
   static asyncrefrestStatus() async {
+     print("Hello############################");
     var contacts =
         (await ContactsService.getContacts(withThumbnails: false)).toList();
+         print("Hello############################");
     var url = 'http://demo6230403.mockable.io/test';
-    var response =
-        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
-    var res = json.decode(response.body);
-    Abc.customerSet.addAll(res["values"]);
+    http.post(url, body: {'name': 'doodle', 'color': 'blue'}).then((Response res){
+           var resp = json.decode(res.body);
+            Abc.customerSet.addAll(resp["values"]);
+        });
+         print("Hello############################");
+ 
+     print("Hello#########################qqq###");
+   
     Abc.contacts = contacts;
+    print("Hello############################");
     return Abc.contacts;
   }
 }
